ceb645924adacda29680a0dda254c88ed73a4177
Kopter <58952825+Kop7er@users.noreply.github.com>
2024-05-09 23:56:00 +0000

    Enabled PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { TanStackRouterVite } from "@tanstack/router-vite-plugin";
+import { VitePWA } from "vite-plugin-pwa";
 import path from "path";
 
 // https://vitejs.dev/config/
@@ -8,7 +9,8 @@ export default defineConfig({
 
     plugins: [
         react(),
-        TanStackRouterVite()
+        TanStackRouterVite(),
+        VitePWA({ registerType: "autoUpdate" })
     ],
 
     resolve: {
